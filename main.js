9da12f669be4caa6de0ac8557a297e743110dd1d
zachatrocity <zachatrocity@gmail.com>
2016-01-19 10:52:21 -0600

    added ui-router, multiple views, and basic architecture

--- main.js
+++ main.js
@@ -8,3 +8,54 @@ win.showDevTools();
 win.on('minimize', function() {
   console.log('Window is minimized');
 });
+
+var easter_egg = new Konami(function() { alert('Konami code!')});
+
+var muonApp = angular.module('muonApp', ["ui.router"])
+    muonApp.config(function($stateProvider, $urlRouterProvider){
+      
+      // For any unmatched url, send to /menu
+      $urlRouterProvider.otherwise("/")
+      
+      $stateProvider
+      	.state('menu', {
+            url: "/",
+            templateUrl: "views/menu.html",
+            controller: function($scope){
+	            
+	          }
+        })
+
+        .state('newgame', {
+            url: "/newgame",
+            templateUrl: "views/newgame.html",
+            controller: function($scope){
+	            $scope.options = ["Novice", "Pro"];
+	          }
+        })
+
+        .state('network', {
+            url: "/network",
+            templateUrl: "views/network.html"
+        })
+
+        .state('howto', {
+            url: "/howto",
+            templateUrl: "views/howto.html"
+        })
+
+        .state('options', {
+            url: "/options",
+            templateUrl: "views/options.html"
+        })
+
+        .state('about', {
+            url: "/about",
+            templateUrl: "views/about.html"
+        })
+
+        .state('board', {
+            url: "/board",
+            templateUrl: "views/board.html"
+        })
+    })
