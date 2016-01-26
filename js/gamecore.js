3c0c228b266ef8d5fcd767b6e001cb134d85f6a6
zachatrocity <zachatrocity@gmail.com>
2016-01-26 17:31:04 -0600

    removed the stupid dumb crazy chars that the slack snippet put in here that broke the whole app

--- js/gamecore.js
+++ js/gamecore.js
@@ -41,7 +41,7 @@ var GameCore = function() {
 	this.GameWon = function() {
 		var winner = '@';
 
-		if (CalculateStateValue() == 100) {
+		if (this.CalculateStateValue() == 100) {
 
 		}
 
@@ -52,12 +52,10 @@ var GameCore = function() {
 
 	var playerOnePosition = 0b00010001011000001000
 	var playerTwoPosition = 0b00000000000110110010
-	​
 	var playerOneFlag = true
 	var playerTwoFlag = true
 	var positionGenes = [2,4,3,4,1]
 	var geneticScalar = 1
-	​
 	// nodeConnections[quad][node]
 	var nodeConnections = [
 		[0b00000000000000011110,0b00000000000100010101,0b00000000000000011011,0b00000010000000010101,0b10000100001000001110], //quad 0 node 0,1,2,3,4
@@ -65,38 +63,31 @@ var GameCore = function() {
 		[0b00000111100000000000,0b01000101010000000000,0b00000110110000000000,0b00000101010000001000,0b10000011101000010000], //quad 2 node 0,1,2,3,4
 		[0b11110000000000000000,0b10101000100000000000,0b11011000000000000000,0b10101000000100000000,0b01110100001000010000]  //quad 3 node 0,1,2,3,4
 	];
-	​
 	this.GetAvailableMoves = function(player, quad, node) {
 		var openPositions = (player == playerTwoPosition) ? playerTwoPosition^playerOnePosition^0b11111111111111111111 : playerOnePosition^playerTwoPosition^0b11111111111111111111;
 		return openPositions&((1<<(quad*5 + node))|nodeConnections[quad][node]);
 	}
-	​
 	this.PerformHumanMove = function(player, quadFrom, nodeFrom, quadTo, nodeTo) {
 		var starting = (1<<(quadFrom*5 + nodeFrom))
 		var ending = (1<<(quadTo*5 + nodeTo))
 		var playerToMove = player == 1 ? playerOnePosition : playerTwoPosition;
-	​
 	 	if(starting&playerToMove && ending&getAvailableMoves(quadFrom, nodeFrom)) {
 	 		player == 1 ? playerOnePosition ^= starting^ending : playerTwoPosition ^= starting^ending;
-	 		console.log("State value: " + calculateStateValue(player, playerToMove, quadTo, nodeTo))
+	 		console.log("State value: " + this.CalculateStateValue(player, playerToMove, quadTo, nodeTo))
 	 	} else {
 
 	 	}
 	}
-	​
 	this.PerformAiMove = function(player, quadFrom, nodeFrom, quadTo, nodeTo) {
 		var playerToMove = player == 1 ? playerOnePosition : playerTwoPosition
-	 	calculateStateValue(player, playerToMove, quadTo, nodeTo);
-	​
+	 	this.CalculateStateValue(player, playerToMove, quadTo, nodeTo);
 	 	//playerToMove is what makes the "physical" board state change.
 	 					//One bit representation of a place on the board
 	 	playerToMove ^= (1<<(quadFrom*5 + nodeFrom))^(1<<(quadTo*5 + nodeTo));
 	}
-	​
 	this.CalculateStateValue = function(player, playerThatMoved, quadTo, nodeTo) {
 		var quad = (playerThatMoved>>5*quadTo)&0b11111;
 		var value = 0;
-	​
 		// Check to see if this move was a winning move.
 		// these are the cases of when a player is moving in their own quadrant (flag status must be checked)
 		if((!(playerOneFlag && player == 1 && quadTo == 2) || (playerTwoFlag && player == 2 && quadTo == 1))
@@ -109,19 +100,17 @@ var GameCore = function() {
 
 		return value;
 	}
-	​
+
 	var start = new Date().getTime();
 	var quadFrom = 2;
 	var quadTo = 0;
 	var nodeFrom = 3;
 	var nodeTo = 3;
 	var limit = 600000;
-
 	for (var i = 0; i < limit; i++) {
-		performAiMove(1,quadTo,nodeTo,quadFrom,nodeFrom)
-		performAiMove(1,quadFrom,nodeFrom,quadTo,nodeTo)
+		this.PerformAiMove(1,quadTo,nodeTo,quadFrom,nodeFrom)
+		this.PerformAiMove(1,quadFrom,nodeFrom,quadTo,nodeTo)
 	}
-
 	var page_load_time = new Date().getTime() - start;
 	//console.log("time for " + (limit*2) + " moves and calculations: " + (page_load_time/1000));
 }
