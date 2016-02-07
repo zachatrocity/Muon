171a7bc4b5f47450b703ee1ec07f3f72ed9f2de2
Zach Russell <zachrussell@Users-MacBook-Pro.local>
2016-02-06 22:36:36 -0600

    made it so that the particle background won't get laggy. For every 4 added i remove 3

--- controllers/MenuCtrl.js
+++ controllers/MenuCtrl.js
@@ -96,7 +96,7 @@ muonApp.controller('MenuCtrl', function ($scope, $stateParams) {
 	        "duration": 0.4
 	      },
 	      "push": {
-	        "particles_nb": 4
+	        "particles_nb": 3
 	      },
 	      "remove": {
 	        "particles_nb": 2
