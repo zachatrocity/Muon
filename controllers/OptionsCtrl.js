42f30cadd1a71cf4e8b027df37bbbdee456f9bfe
Jordan Merrick <jmerrick@harding.edu>
2016-02-21 15:47:10 -0600

    Added the ability to mute the music from the options menu.

--- controllers/OptionsCtrl.js
+++ controllers/OptionsCtrl.js
@@ -1,3 +1,19 @@
 muonApp.controller('OptionsCtrl', function ($scope, $stateParams) {
 	
+	$scope.toggleSound = function() {
+		var muteText = document.getElementById("mutebtn").innerHTML;
+		var muteFalseText = 'Mute Sound &nbsp<span class="batch" data-icon="&#xf03b"></span>';
+		var muteTrueText = 'Unmute Sound &nbsp<span class="batch" data-icon="&#xf038"></span>';
+		
+		if (muteText.indexOf("Unmute") == -1)
+		{
+			Audio.background.mute();
+			document.getElementById("mutebtn").innerHTML = muteTrueText;
+		}
+		else
+		{
+			Audio.background.unmute();
+			document.getElementById("mutebtn").innerHTML = muteFalseText;
+		}
+	}
 });
