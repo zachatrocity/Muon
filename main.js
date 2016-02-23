33fd5ac33ba7ace5ebbd220f91109f91476fce14
Zach Russell <zachrussell@Users-MacBook-Pro.local>
2016-02-23 14:09:31 -0600

    Added the ability to hit enter to create username

--- main.js
+++ main.js
@@ -237,6 +237,18 @@ var muonApp = angular.module('muonApp', ["ui.router", "ngAnimate"])
       Audio.togglesound = resp.title;
     }
   });
-
-  
 })
+
+muonApp.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
