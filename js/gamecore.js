97513334ed1b2cd2f528d1618fe104600b44acc8
zachatrocity <zachatrocity@gmail.com>
2016-04-05 09:52:45 -0500

    fixed the broken functionality of the flags

--- js/gamecore.js
+++ js/gamecore.js
@@ -89,19 +89,24 @@ var gameCore = {
 			if(character == gameCore.network.team){
 				gameCore.network.localPos ^= bitFrom ^ bitTo;
 				gameCore.AddMoveToHistory(new Move(from, to, "local"));
+				//remove my flag if needed
+				if(evaluation.isHomeQuadEmpty((gameCore.network.role == 'host') ? 2 : 1, gameCore.network.localPos)) {
+					gameCore.network.localFlag = false;
+					BoardGUI.removeAntiMuonFlag();
+					console.log("Player can now win from their home quad");
+				}
 			} else {
 				gameCore.network.opponentPos ^= bitFrom ^ bitTo;
 				gameCore.AddMoveToHistory(new Move(from, to, "opponent"));
+				//remove opponent flag if needed
+				if(evaluation.isHomeQuadEmpty((gameCore.network.role == 'host') ? 1 : 2, gameCore.network.opponentPos)) {
+					gameCore.network.opponentFlag = false;
+					BoardGUI.removeMuonFlag();
+					console.log("Opponent can now win from their home quad");
+				}
 			}
 
 			gameCore.board.moveMuonTweenFoci(from, to);
-
-			//remove my flag if needed
-			if(evaluation.isHomeQuadEmpty((gameCore.network.role == 'host') ? 2 : 1, gameCore.network.localPos)) {
-				gameCore.network.localFlag = false;
-				BoardGUI.removeAntiMuonFlag();
-				console.log("Player can now win from their home quad");
-			}
 		},
 		MakeOpponentMove: function(from, to){
 			var bitFrom = convert.intToBit(from);
